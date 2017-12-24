16990b675886757c2660fb3c7d229afd31bf0fa6
niugengyun <515343908@qq.com>
2017-12-24 12:11:15 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -1,24 +1,23 @@
 <?php
+
 namespace App\Http\Controllers\Wechat;
 
-use EasyWeChat\Kernel\Contracts\EventHandlerInterface;
-use EasyWeChat\Kernel\Messages\Message;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Handler\TextMessageHandler;
 use App\Handler\EventMessageHandler;
+use App\Http\Controllers\Controller;
+use EasyWeChat\Kernel\Messages\Message;
 
 class WechatController extends Controller
 {
-
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function serve()
     {
         $app = app('wechat.official_account');
-        $app->server->push(TextMessageHandler::class,Message::TEXT);
-        $app->server->push(EventMessageHandler::class,Message::EVENT);
+        $app->server->push(TextMessageHandler::class, Message::TEXT);
+        $app->server->push(EventMessageHandler::class, Message::EVENT);
+
         return $app->server->serve();
     }
 }
