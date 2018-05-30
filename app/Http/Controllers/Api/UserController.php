ed4fecd63f078fb459706cde5b1b55b670353d7f
cnsecer <515343908@qq.com>
2018-05-30 19:20:00 +0800

    完善后台功能

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -26,6 +26,7 @@ class UserController extends Controller
     }
 
     /**
+     * 用户列表
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
